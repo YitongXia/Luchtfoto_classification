b7328d507acc13cb455678211748c9d03e3a0946
YitongXia <xiayitong0630@gmail.com>
2022-08-14 18:18:30 +0200

    new update single rooftop classification (unsupervised)

--- resources/pixel_based_extraction.py
+++ resources/pixel_based_extraction.py
@@ -15,13 +15,19 @@ import rasterio.warp
 # @ use rasterio to read and store the image
 # @ return
 # @ numpy array with every band of the image
-def read_single_raster(file_name):
-    with rasterio.open(file_name) as dataset:
+def read_single_raster(filename):
+    with rasterio.open(filename) as dataset:
         image_band = []
+        print(dataset.indexes)
         for i in range(len(dataset.indexes)):
-            image_band.append(dataset.read(1))
+            image_band.append(dataset.read(i + 1))
     return image_band
 
+
+# --*** read images ***----
+# @ use rasterio to read and store the image
+# @ return
+# @ numpy array with every band of the image
 
 def read_raster_folder(input_folder):
     # function to read the file and give the info needed
@@ -30,31 +36,17 @@ def read_raster_folder(input_folder):
     print(input_folder)
 
     raster_collection = []
-    # loop trough files and retreive objects info as well as loose point info for output
+    # loop trough files and retrieve objects info as well as loose point info for output
     for file in os.listdir(input_folder):
         raster_collection.append(read_single_raster(file))
     return raster_collection
 
 
+# plot the classification result in 3D space based on spectral features
+# input: X, Y, Z and y_prediction
+# output: plot
 
-# @ auto classification for each rooftop
-# input:tif file of each rooftop
-# output: (R,G,B) spectral feature of the rooftop
-
-def single_classification(file_name):
-
-    roof = []
-    roof = read_single_raster(file_name)
-    # for band in range(len(roof)):
-    X = roof[0].flatten()
-    Y = roof[1].flatten()
-    Z = roof[2].flatten()
-    roof_rgb = []
-    for i in range(len(roof[0])):
-        roof_rgb.append([X[i], Y[i], Z[i]])
-
-    y_pred = KMeans(n_clusters=9, random_state=5).fit_predict(roof_rgb)
-
+def draw_plot(X, Y, Z, y_pred):
     ax = plt.axes(projection='3d')
     ax.scatter3D(X, Y, Z, c=y_pred)
 
@@ -68,6 +60,68 @@ def single_classification(file_name):
     ax.set_ylabel("G")
     ax.set_zlabel("B")
     plt.show()
+
+
+# auto classification for each rooftop
+# input:tif file of each rooftop
+# output: (R,G,B) spectral feature of the rooftop
+
+def single_classification(file_name, n_cluster):
+    roof = []
+    roof = read_single_raster(file_name)
+
+    # for band in range(len(roof)):
+    X_old = roof[0].flatten()
+    Y_old = roof[1].flatten()
+    Z_old = roof[2].flatten()
+    roof_rgb = []
+    X = []
+    Y = []
+    Z = []
+    for i in range(len(X_old)):
+        if X_old[i] == 256 & Y_old[i] == 256 & Z_old[i] == 256:
+            continue
+        else:
+            roof_rgb.append([X_old[i], Y_old[i], Z_old[i]])
+            X.append(X_old[i])
+            Y.append(Y_old[i])
+            Z.append(Z_old[i])
+
+    print("the number of valid pixel is ",len(X))
+    # clean no_data value (as 256)
+    y_pred = KMeans(n_clusters=n_cluster, random_state=5).fit_predict(roof_rgb)
+    # draw_plot(X,Y,Z,y_pred)
+
+    a1 = 0
+    a2 = 0
+    a3 = 0
+    cluster = 0
+    for item in y_pred:
+        if item == 1:
+            a1 += 1
+        elif item == 2:
+            a2 += 1
+        elif item == 3:
+            a3 += 1
+
+    if a1 >= a2:
+        cluster = 1 if a1 >= a3 else 2
+    else:
+        cluster = 2 if a2 >= a3 else 3
+
+    print("the main cluster is ", cluster)
+    majority_pixel = [0, 0, 0]
+    count=0
+    for i in range(len(y_pred)):
+        if y_pred[i] == cluster:
+            majority_pixel[0] += X[i]
+            majority_pixel[1] += Y[i]
+            majority_pixel[2] += Z[i]
+            count+=1
+    majority_pixel = [majority_pixel[0] / count,
+                      majority_pixel[1] / count,
+                      majority_pixel[2] / count]
+    return majority_pixel
 
 
 # function to read the file and give the info needed
@@ -97,44 +151,19 @@ def read(file_route):
 
 
 def kmeans(X, Y, Z, roofs_rgb):
-    y_pred = KMeans(n_clusters=9, random_state=5).fit_predict(roofs_rgb)
-
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(X, Y, Z, c=y_pred)
-
-    # set axes limits
-    ax.set_xlim(0, 255)
-    ax.set_ylim(0, 255)
-    ax.set_zlim(0, 255)
-
-    # set labels
-    ax.set_xlabel("R")
-    ax.set_ylabel("G")
-    ax.set_zlabel("B")
-    plt.show()
+    y_pred = KMeans(n_clusters=3, random_state=5).fit_predict(roofs_rgb)
+    draw_plot(X, Y, Z, y_pred)
 
 
 def dbscan(X, Y, Z, roofs_rgb):
     y_pred = DBSCAN(eps=3, min_samples=15).fit_predict(roofs_rgb)
-
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(X, Y, Z, c=y_pred)
-
-    # set axes limits
-    ax.set_xlim(0, 255)
-    ax.set_ylim(0, 255)
-    ax.set_zlim(0, 255)
-
-    # set labels
-    ax.set_xlabel("R")
-    ax.set_ylabel("G")
-    ax.set_zlabel("B")
-    plt.show()
+    draw_plot(X, Y, Z, y_pred)
 
 
 if __name__ == '__main__':
-    file_name = r"..\dataset" + r"\amsterdam.tif"
+    file_name = r"..\dataset\segmentation" + r"\2.tif"
 
     input_folder = os.getcwd() + r"..\dataset\segmentation"
 
-    read_raster(file_name)
+    major = single_classification(file_name, 5)
+    print(major)
